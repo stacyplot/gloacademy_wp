7cb3be59e020c80dbd1e4b38ce9162e679ceb74d
stacyplot <stacyplot@gmail.com>
2021-03-03 01:50:35 +0300

    Сделан Hero Section

---
+++ universal_theme/front-page.php
@@ -0,0 +1,75 @@
+<?php get_header();?>
+<main class="front-page-header">
+  <div class="container">
+    <div class="hero">
+      <div class="left">
+        <?php
+          global $post;
+
+          $myposts = get_posts([ 
+            'numberposts' => 1,
+            'category_name' => 'java-script'
+          ]);
+
+          if( $myposts ){
+            foreach( $myposts as $post ){
+              setup_postdata( $post );
+              ?>
+        <!-- Вывода постов, функции цикла: the_title() и т.д. -->
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="post-thumb">
+        <?php $author_id = get_the_author_meta('ID'); ?>
+        <a href="<?php echo get_author_posts_url($author_id); ?>" class="author">
+          <img src="<?php echo get_avatar_url($author_id); ?>" alt="" class="avatar">
+          <div class="author-bio">
+            <span class="author-name"><?php the_author(); ?></span>
+            <span class="author-rank">Роль автора</span>
+          </div>
+        </a>
+        <div class="post-text">
+          <?php the_category();?>
+          <h2 class="post-title"><?php the_title();?></h2>
+          <a href="<?php echo get_the_permalink();?>" class="more">Читать далее</a>
+        </div>
+      <?php 
+      } 
+    } else {
+      ?> <p>Постов нет</p> <?php
+    }
+
+    wp_reset_postdata(); // Сбрасываем $post
+    ?>
+      </div>
+      <div class="right">
+        <h3 class="recommend">Рекомендуем</h3>
+        <ul class="posts-list">
+          <?php
+            global $post;
+
+            $myposts = get_posts([ 
+              'numberposts' => 5,
+              'offset' => 1,
+            ]);
+
+            if( $myposts ){
+              foreach( $myposts as $post ){
+                setup_postdata( $post );
+                ?>
+                <!-- Вывода постов, функции цикла: the_title() и т.д. -->
+                <li class="post">
+                  <?php the_category();?>
+                  <a class="post-permalink" href="<?php echo get_the_permalink();?>">
+                    <h4 class="post-title"><?php the_title();?></h4>
+                  </a>
+                </li>
+          <?php 
+              } 
+            } else {
+              ?> <p>Постов нет</p> <?php
+            }
+            wp_reset_postdata(); // Сбрасываем $post
+            ?>
+        </ul>
+      </div>
+    </div>
+  </div>
+</main>
---
+++ universal_theme/functions.php
@@ -0,0 +1,34 @@
+<?php
+// Добавление расширенных возможностей
+if ( ! function_exists( 'universal_theme_setup' ) ) :
+  function universal_theme_setup() {
+    // Добавление тега title 
+    add_theme_support( 'title-tag' );
+
+    // Добавление миниатюр
+    add_theme_support( 'post-thumbnails', array( 'post' ) );          // Только для post
+    
+    // Добавление логотипа
+    add_theme_support( 'custom-logo', [
+    'width'       => 163,
+    'flex-height' => true,
+    'header-text' => 'Universal',
+    'unlink-homepage-logo' => false, // WP 5.5
+  ] );
+
+    // Регистрация меню
+    register_nav_menus( [
+      'header_menu' => 'Меню в шапке',
+      'footer_menu' => 'Меню в подвале'
+    ] );
+}
+endif;
+add_action( 'after_setup_theme', 'universal_theme_setup' );
+
+// Подключение стилей и скриптов
+function enqueue_universal_style() {
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style( 'universal-theme-style', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style');
+  wp_enqueue_style('Roboto-Slab', 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap');
+} 
+add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
